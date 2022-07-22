fed2719d970664df76c7979cad074e820ce30712
Nikita <nikitann2501@gmail.com>
2022-07-22 20:03:46 +0530

    heroku modifications

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 require('./models/db');
-const express= require("express");
-const dotenv =require("dotenv");
-dotenv.config({path:'./.env'});
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: './.env' });
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const port = process.env.PORT;
-
+const path = require("path");
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, "client", "build")));
 const userRouter = require('./Routers/userRoutes');
 
 app.use('/user', userRouter);
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
