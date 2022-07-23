31f85133ce4e2bf68f220fa9a341787009a9aca9
Nikita <nikitann2501@gmail.com>
2022-07-23 15:27:15 +0530

    .

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const dotenv = require("dotenv");
 dotenv.config({ path: './.env' });
 const bodyParser = require("body-parser");
 const cors = require('cors');
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 const path = require("path");
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "client", "build")));
 const userRouter = require('./Routers/userRoutes');
 
 app.use('/user', userRouter);
+app.use(express.static(path.join(__dirname, "client", "build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
